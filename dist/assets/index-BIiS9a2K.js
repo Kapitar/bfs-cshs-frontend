23c4faed84194240b6da62dc00169d7e5136cfc5
kapitar <kkapitar@ya.ru>
2024-04-04 00:05:57 +0900

    adding dist

--- dist/assets/index-BIiS9a2K.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/ColorUpdater-DbGM4r9n.js","assets/index-BMlR1jPU.js","assets/index-DqWS7ZOo.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as e}from"./index-BMlR1jPU.js";async function i(r,o=!0){await r.addParticleUpdater("color",async t=>{const{ColorUpdater:a}=await e(()=>import("./ColorUpdater-DbGM4r9n.js"),__vite__mapDeps([0,1,2]));return new a(t)},o)}export{i as loadColorUpdater};
--- dist/assets/index-BSfIyqzT.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/Collider-B0jm4iyK.js","assets/index-BMlR1jPU.js","assets/index-DqWS7ZOo.css","assets/ParticlesInteractorBase-vfDeBun3.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as a}from"./index-BMlR1jPU.js";async function n(t,o=!0){await t.addInteractor("particlesCollisions",async i=>{const{Collider:r}=await a(()=>import("./Collider-B0jm4iyK.js"),__vite__mapDeps([0,1,2,3]));return new r(i)},o)}export{n as loadParticlesCollisionsInteraction};
