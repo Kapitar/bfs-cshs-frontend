d9ecc6430c96ec775df412609e2da98c984e7c46
kapitar <kkapitar@ya.ru>
2024-04-25 08:28:08 +0900

    change the about content

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,26 @@
 <template>
     <div class="container">
         <h1 class="title text-center" id="about-title"></h1>
-        <h2>Our mission</h2>
         <p>The Computer Science Honor Society at BFS is committed to promoting excellence in computer science education
-            and recognizing high school students who exhibit high academic and personal achievements in the field. We
-            are proud to be a part of the nationwide network of honor societies supported by the Computer Science
-            Teachers Association (CSTA).</p>
+            and recognizing high school students who excel academically and personally in this field. We proudly align
+            ourselves with the nationwide network of honor societies supported by the Computer Science Teachers
+            Association (CSTA).
+        </p>
+        <br>
+        <p>
+            Our mission extends beyond academic achievement; we embrace the core values of our schoolwide learner
+            outcomes. We strive to honor students who not only demonstrate academic prowess but also exhibit social
+            consciousness, diligence, effective communication, respect, and a passion for lifelong learning.
+
+        </p>
+        <br>
+        <p>
+            As members of our society, we pledge to embody these values within our organization and throughout our
+            community. We seek to contribute positively to computer science education and ethical technological
+            advancement. In collaboration with CSTA, we aim to cultivate a supportive environment that fosters the
+            growth of future leaders in computer science, driven by a sense of social responsibility and a commitment to
+            ongoing personal and academic development.
+        </p>
     </div>
 </template>
 
